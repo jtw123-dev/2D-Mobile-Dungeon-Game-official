ced123a1fac931728e6d68c43a0ce9bd8bdd7fb6
jtw123-dev <78871610+jtw123-dev@users.noreply.github.com>
2021-09-30 19:40:35 -0400

    Finished everything in Player Progression but did not add material see last video in section

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Player.cs
@@ -7,20 +7,33 @@ public class Player : MonoBehaviour
     private Rigidbody2D _body;
     [SerializeField]private float _jumpForce=10;
     [SerializeField] private float _speed;
-    private PlayerAnimation _anim;
+    private PlayerAnimation _playerAnim;
     private SpriteRenderer _renderer;
+    private SpriteRenderer _swordRenderer;
+    private Vector3 _swordPos;
+    private Animator _anim;
+    private bool _grounded=false;
+    private bool _resetJump;
 
     // Start is called before the first frame update
     void Start()
     {
+        _anim = GameObject.Find("Sprite").GetComponent<Animator>();
         _body = GetComponent<Rigidbody2D>();
-        _anim = GameObject.Find("Sprite").GetComponent<PlayerAnimation>();
-        _renderer = GameObject.Find("Sprite").GetComponent<SpriteRenderer>();
+        _swordRenderer = GameObject.Find("Sword_arc").GetComponent<SpriteRenderer>();
+        _playerAnim = GameObject.Find("Sprite").GetComponent<PlayerAnimation>();
+        _renderer = GameObject.Find("Sprite").GetComponent<SpriteRenderer>(); //flip it just like sword
+        _swordPos = GameObject.Find("Sword_arc").transform.position;
     }
     // Update is called once per frame
     void Update()
     {
         Movement();
+
+        if (IsGrounded() == true && Input.GetMouseButtonDown(0))
+        {
+            _playerAnim.Attack();
+        }
     }
 
     private void FixedUpdate()
@@ -30,34 +43,51 @@ public class Player : MonoBehaviour
     private void Movement()
     {
         float _horizontalInput = Input.GetAxisRaw("Horizontal");
-        
-        _anim.Move(_horizontalInput);
+        _grounded = IsGrounded();
+
+        _playerAnim.Move(_horizontalInput);
         if (_horizontalInput>0)
         {
+            _swordRenderer.flipX = false;
+            _swordRenderer.flipY = false;
+            _swordRenderer.transform.localPosition = new Vector3(1, 0.09f, 0.25f);
             _renderer.flipX = false;
         }
         else if (_horizontalInput<0)
         {
+            _swordRenderer.flipX = true;
+            _swordRenderer.flipY = true;
+            _swordRenderer.transform.localPosition = new Vector3(-1, -0.09f, 0.25f);
             _renderer.flipX = true;
         }
         if (IsGrounded() == true && Input.GetKeyDown(KeyCode.Space))
         {
             _body.velocity = new Vector2(_body.velocity.x, _jumpForce);
+            StartCoroutine("ResetJumpRoutine");
+            _playerAnim.Jump(true);
         }
         _body.velocity = new Vector2(_horizontalInput * _speed, _body.velocity.y );     
     }
     bool IsGrounded()
     {
-        float rayLength = 0.7f;
+        float rayLength = 1f;
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,Vector2.down,rayLength);
-
+        Debug.DrawRay(transform.position, Vector2.down, Color.white);
+        
         if (hitInfo.collider!=null)
         {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+            if (_resetJump==false)
+            
+                _playerAnim.Jump(false);
+                return true;              
+            }
+         
+            return false;      
+    }
+    private IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.7f);
+        _resetJump = false;
     }
 }
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/PlayerAnimation.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/PlayerAnimation.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     private Animator _anim;
+    private Animator _swordAnim;
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _swordAnim = GameObject.Find("Sword_arc").GetComponent<Animator>();
     }
     public void Move(float move)
     {
         _anim.SetFloat("Move", Mathf.Abs (move));
+    }
+    public void Jump(bool jumping)
+    {
+        _anim.SetBool("Jumping", jumping);
+    }  
+
+    public void Attack ()
+    {
+        _anim.SetTrigger("Attack");
+        _swordAnim.SetTrigger("SwordAnimation");
     }
 }
